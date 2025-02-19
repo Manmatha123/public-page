fb94c225375d701bb3c2c30b9066df84c61951a9
Manmatha Mandal <manmatha.mandal@novagenixlab.com>
2025-02-19 09:40:41 +0530

    changes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './homepage/HomePage'
+import ProductView from './productView/ProductView'
 
 function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/view" element={<ProductView />} />
         </Routes>
     </>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
             <footer>
-                <div className="ft-text">&copy; 2024 <i class="fa-sharp fa-regular fa-copyright"></i> All rights reserved | made with <i class="fa-sharp fa-solid fa-heart" id="heart"></i> by team by Clg</div>
+                <div className="ft-text">@ 2024 <i class="fa-sharp fa-regular fa-copyright"></i>All rights reserved | made with <i class="fa-sharp fa-solid fa-heart" id="heart"></i> by Team-A College</div>
             </footer>
         </>
     )
